a408f90279c88c69d6b182366b5d57a7f7da262d
marc <marc@marcotto.de>
2018-03-05 22:42:23 +0100

    Code from chapter 9

--- game.py
+++ game.py
@@ -1,37 +1,9 @@
-class Weapon:
-    def __init__(self):
-        raise NotImplementedError("Do not create raw Weapon objects.")
-
-    def __str__(self):
-        return self.name
-
-
-class Rock(Weapon):
-    def __init__(self):
-        self.name = "Rock"
-        self.description = "A fist-sized rock, suitable for bludgeoning."
-        self.damage = 5
-
-
-class Dagger(Weapon):
-    def __init__(self):
-        self.name = "Dagger"
-        self.description = "A small dagger with some rust. " \
-                           "Somewhat more dangerous than a rock."
-        self.damage = 10
-
-
-class RustySword(Weapon):
-    def __init__(self):
-        self.name = "Rusty sword"
-        self.description = "This sword is showing its age, " \
-                           "but still has some fight in it."
-        self.damage = 20
+from player import Player
 
 
 def play():
-    inventory = [Rock(), Dagger(), 'Gold(5)', 'Crusty Bread']
     print("Escape from Cave Terror!")
+    player = Player()
     while True:
         action_input = get_player_command()
         if action_input in ['n', 'N']:
@@ -43,11 +15,7 @@ def play():
         elif action_input in ['w', 'W']:
             print("Go West!")
         elif action_input in ['i', 'I']:
-            print("Inventory:")
-            for item in inventory:
-                print('* ' + str(item))
-            best_weapon = most_powerful_weapon(inventory)
-            print("Your best weapon is your {}".format(best_weapon))
+            player.print_inventory()
         else:
             print("Invalid action!")
 
@@ -55,18 +23,5 @@ def play():
 def get_player_command():
     return input('Action: ')
 
-
-def most_powerful_weapon(inventory):
-    max_damage = 0
-    best_weapon = None
-    for item in inventory:
-        try:
-            if item.damage > max_damage:
-                best_weapon = item
-                max_damage = item.damage
-        except AttributeError:
-            pass
-
-    return best_weapon
 
 play()
